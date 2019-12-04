1d72e8ee8ac8070492f1e1c852ad1db831a470b6
Marc Lozier <marc.lozier@ontariovirtualschool.ca>
2019-12-04 16:27:36 -0500

    included js file

---
+++ bookmarklet.js
@@ -0,0 +1,2 @@
+//Don't forget to include the javascript: in the bookmark
+var p=window.performance.getEntries(),urls=[];if(p.forEach(function(e){e.name.indexOf("m3u8")>0&&e.name.indexOf("index_0")>0&&urls.push(e.name)}),1==urls.length){var url=urls[0].substring(0,urls[0].indexOf("?"));navigator.clipboard.writeText(url),alert("Video URL Copied")}else urls.length>1?(console.log(urls),alert("Multuple URLs detected - see console")):alert("No video detected - play the video first");
